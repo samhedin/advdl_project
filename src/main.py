11bfdb2c10ec910238462d4c2dbbcc78b08c1bd7
samhedin <sam.hedin@gmail.com>
2021-11-10 22:56:51 +0100

    add "proper convuliton"

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
 matplotlib.use("TkAgg")
+from scipy import signal
 
 from src.dataset import build_dataset
 
@@ -25,8 +26,21 @@ def showimg(image, filepath=None):
     else:
         plt.show()
 
-def smooth(image):
-    return image
+# noise 0.5 taken from original paper.
+def smooth(image, noise=0.5, proper_convolution=False):
+    # Returns a tensor with the same size as input that is filled
+    # with random numbers from a normal distribution with mean 0 and variance 1.
+    if not proper_convolution:
+        return image + torch.randn_like(image) * noise
+    else: # https://scipy-lectures.org/intro/scipy/auto_examples/solutions/plot_image_blur.html
+        # First a 1-D  Gaussian
+        t = np.linspace(-3, 3, 6)
+        bump = np.exp(-0.1*t**2)
+        bump /= np.trapz(bump) # normalize the integral to 1
+
+        # make a 2-D kernel out of it
+        kernel = bump[:, np.newaxis] * bump[np.newaxis, :]
+        return signal.fftconvolve(image, kernel[:, :, np.newaxis], mode='same')
 
 def main():
     config = Config()
@@ -36,8 +50,13 @@ def main():
     for images, labels in train_loader:
         img = images[0]
         showimg(img, filepath="imgs/before.png")
+
         after_img = smooth(img)
         showimg(after_img, filepath="imgs/after.png")
+
+
+        after_img_conv = smooth(img, proper_convolution=True)
+        showimg(after_img_conv, filepath="imgs/after_conv.png")
         break
 
 
