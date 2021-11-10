a9582c342fad7476dccc0c6691bf07d3843ed5c6
samhedin <sam.hedin@gmail.com>
2021-11-10 23:22:37 +0100

    use CIFAR10

--- src/main.py
+++ src/main.py
@@ -1,34 +1,37 @@
 from dataclasses import dataclass
 
 import torch
+
 torch.manual_seed(42)
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-matplotlib.use("TkAgg")
-from scipy import signal
-from scipy.ndimage import gaussian_filter
 
+matplotlib.use("TkAgg")
+from scipy.ndimage import gaussian_filter
 from src.dataset import build_dataset
+
 
 @dataclass
 class Config:
-    data_root: str = './data'
-    dataset: str = "MNIST"
+    data_root: str = "./data"
+    dataset: str = "CIFAR10"
     batch_size: int = 64
 
 
 def showimg(image, filepath=None):
     image = np.array(image)
-    image = np.reshape(image, (28,28))
-    plt.imshow(image, cmap="Greys_r")
+    print(image.shape)
+    image = np.transpose(image, (1,2,0))
+    plt.imshow(image)
     if filepath:
         plt.savefig(filepath)
     else:
         plt.show()
 
+
 # noise 0.5 taken from original paper.
-def smooth(image, noise=0.5, proper_convolution=False):
+def smooth(image, noise=0.1, proper_convolution=False):
     # Returns a tensor with the same size as input that is filled
     # with random numbers from a normal distribution with mean 0 and variance 1.
     if not proper_convolution:
@@ -36,7 +39,8 @@ def smooth(image, noise=0.5, proper_convolution=False):
     else:
         # https://scipy-lectures.org/intro/scipy/auto_examples/solutions/plot_image_blur.html
         # or: https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.gaussian_filter.html
-        return gaussian_filter(image, sigma=1.5)
+        return gaussian_filter(image, sigma=1.2)
+
 
 def main():
     config = Config()
@@ -50,11 +54,10 @@ def main():
         after_img = smooth(img)
         showimg(after_img, filepath="imgs/after.png")
 
-
         after_img_conv = smooth(img, proper_convolution=True)
         showimg(after_img_conv, filepath="imgs/after_conv.png")
         break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
