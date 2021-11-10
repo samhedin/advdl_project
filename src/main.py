99f901e6c637e3be0e9c9651b57aaf1e87b7dd14
samhedin <sam.hedin@gmail.com>
2021-11-10 22:32:08 +0100

    Add showimg function

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 
 import torch
 torch.manual_seed(42)
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
 
 from src.dataset import build_dataset
 
@@ -11,12 +15,29 @@ class Config:
     dataset: str = "MNIST"
     batch_size: int = 64
 
+
+def showimg(image, filepath=None):
+    image = np.array(image)
+    image = np.reshape(image, (28,28))
+    plt.imshow(image, cmap="Greys_r")
+    if filepath:
+        plt.savefig(filepath)
+    else:
+        plt.show()
+
+def smooth(image):
+    return image
+
 def main():
     config = Config()
     train_loader, test_loader = build_dataset(config)
 
+    # show an image
     for images, labels in train_loader:
-        import pdb; pdb.set_trace()
+        img = images[0]
+        showimg(img, filepath="imgs/before.png")
+        after_img = smooth(img)
+        showimg(after_img, filepath="imgs/after.png")
         break
 
 
