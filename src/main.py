21a8920fe2314cf8184875431fdf21926d0c6089
samhedin <sam.hedin@gmail.com>
2021-11-19 15:43:38 +0100

    argparser

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import argparse
 import torch
 
 from src.model import PixelCNN
@@ -64,6 +65,38 @@ def show_image(train_loader):
         showimg(after_img_conv, filepath="imgs/after_conv.png")
         break
 
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--data_dir', type=str,
+                        default='data', help='Location for the dataset')
+    parser.add_argument('-o', '--save_dir', type=str, default='models',
+                        help='Location for parameter checkpoints and samples')
+    parser.add_argument('-d', '--dataset', type=str,
+                        default='cifar', help='Can be either cifar|mnist')
+    parser.add_argument('-p', '--print_every', type=int, default=50,
+                        help='how many iterations between print statements')
+    parser.add_argument('-t', '--save_interval', type=int, default=10,
+                        help='Every how many epochs to write checkpoint/samples?')
+    parser.add_argument('-r', '--load_params', type=str, default=None,
+                        help='Restore training from previous model checkpoint?')
+    # model
+    parser.add_argument('-q', '--nr_resnet', type=int, default=5,
+                        help='Number of residual blocks per stage of the model')
+    parser.add_argument('-n', '--nr_filters', type=int, default=160,
+                        help='Number of filters to use across the model. Higher = larger model.')
+    parser.add_argument('-m', '--nr_logistic_mix', type=int, default=10,
+                        help='Number of logistic components in the mixture. Higher = more flexible model')
+    parser.add_argument('-l', '--lr', type=float,
+                        default=0.0002, help='Base learning rate')
+    parser.add_argument('-e', '--lr_decay', type=float, default=0.999995,
+                        help='Learning rate decay, applied every step of the optimization')
+    parser.add_argument('-b', '--batch_size', type=int, default=64,
+                        help='Batch size during training per GPU')
+    parser.add_argument('-x', '--max_epochs', type=int,
+                        default=5000, help='How many epochs to run in total?')
+    parser.add_argument('-s', '--seed', type=int, default=1,
+                        help='Random seed to use')
+    return parser.parse_args()
 
 if __name__ == "__main__":
     config = Config()
