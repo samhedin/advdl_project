43c7153ef96354ac30313d448fd4c4105e1d4577
erikdao <cuong@neuraltalks.io>
2021-12-06 10:01:55 +0100

    WIP: debugging SSD

--- src/sampler.py
+++ src/sampler.py
@@ -17,14 +17,14 @@ def single_step_denoising(
     x_tilde = module.sample(sample_batch_size).to(module.device)
 
     # Log PDF:
-    logits = module.model(x_tilde).detach()
+    x_t = autograd.Variable(x_tilde, requires_grad=True).to(module.device)
+    logits = module.model(x_t).detach()
     # Require grad for x_tilde so we can get its gradients later
-    x_tilde.requires_grad_(True)
-    log_pdf = mix_logistic_loss(x_tilde, logits, likelihood=True)
-
+    # x_tilde.requires_grad_(True)
+    log_pdf = mix_logistic_loss(x_t, logits, likelihood=True)
     # Compute the gradient of the loss w.r.t x_tilde
-    nabla = autograd.grad(log_pdf.sum(), x_tilde, create_graph=True)[0]
-    x_bar = x_tilde + noise ** 2 * nabla
+    nabla = autograd.grad(log_pdf.sum(), x_t, create_graph=True)[0]
+    x_bar = x_tilde + (noise ** 2) * nabla
     x_bar = x_bar.detach().data
     x_tilde = x_tilde.detach().data
     return x_bar, x_tilde
